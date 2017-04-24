e3e2aad63a3e487899249b920aad78f968336b20
918273244 <918273244@qq.com>
2017-04-24 17:43:51 +0800

    添加translate

--- steven-gantt/src/main/java/com/steven/gantt/App.java
+++ steven-gantt/src/main/java/com/steven/gantt/App.java
@@ -2,12 +2,14 @@ package com.steven.gantt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 启动
  * Created by Steven on 2017/4/12.
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class App {
 
     public static void main(String[] args) {
