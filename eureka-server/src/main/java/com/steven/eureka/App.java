76b73a998dd5e4b27178db35a2c2c028e5ceaf66
918273244 <918273244@qq.com>
2017-05-03 10:31:43 +0800

    添加spring cloud应用

---
+++ eureka-server/src/main/java/com/steven/eureka/App.java
@@ -0,0 +1,19 @@
+package com.steven.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现。
+ * Created by Steven on 2017/5/2.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
